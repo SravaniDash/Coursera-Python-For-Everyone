65fc3b64e6e21493375ce7448ede4425b5a98794
Sravani Dash <sravanipoornimadash@gmail.com>
2020-11-25 13:42:51 -0500

    all exercises up to ch 10

---
+++ exercises/chapter3/3-1.py
@@ -0,0 +1,22 @@
+# 3.1 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Pay the hourly rate for
+# the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours. Use 45 hours and a rate of 10.50 per hour
+# to test the program (the pay should be 498.75). You should use input to read a string and float() to convert the string to a
+#number. Do not worry about error checking the user input - assume the user types numbers properly.
+
+h = input('Enter hours:')
+fh = float(h)
+
+r = input('Enter rate per hour:')
+fr = float(r)
+
+if(fh > 40):
+    #print('Overtime')
+    regPay = fr * 40
+    extraHours = fh - 40
+    extraPay = extraHours * (fr * 1.5)
+    pay = regPay + extraPay
+
+else:
+    #print('Regular')
+    pay = fh * fr
+print(pay)
