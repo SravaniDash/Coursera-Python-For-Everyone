c91868b3a8f6dea304a8f7e6dc754f1d280e6ad1
Sravani Dash <sravanipoornimadash@gmail.com>
2020-12-20 21:44:11 -0500

    chapter 11 and 12 exercises

---
+++ exercises/chapter11/regEx.py
@@ -0,0 +1,11 @@
+import re
+
+count = 0
+hand = open("regex_sum.txt")
+lines = hand.read()
+match = re.findall('[0-9]+', lines)
+for i in match:
+    number = int(i)
+    count = count + number
+
+print(count)
